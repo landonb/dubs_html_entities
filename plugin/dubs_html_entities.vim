landonb/dubs_html_entities

commit 3b14aa2a5f784eb52ee0fc630bf5fd4fa0a4037e

Update: Use <Leader>d prefix — \dh opens HTML table, \dH runs prompt
- Also remove the <Alt-Shift-5> entity table toggle,
  and reassign to \dh.

  - The previous \ht and <Alt-Shift-5> commands were essentially
    redundant (the latter would toggle it open/closed, and the
    former would just open it (and then you had to 'q' to close
    it, or just close the window).

plugin/dubs_html_entities.vim
@@ -95,29 +95,31 @@ let plugin_htmlchartable_vim = 1
 "       might really be <SNR>16_QuickLookup())
 
 " ---------------
-" Open HTML Character Entity Table with <Leader>dh
-" - HSTRY/2024-12-09: Was <Leader>ht but this feature is rarely
-"   used, and I've been moving most Dubs maps under \d prefix.
-if !hasmapto('<Plug>DubsHtmlEntities_HtmlCharTable')
-  map <silent> <unique> <Leader>dh
-    \ <Plug>DubsHtmlEntities_HtmlCharTable
-endif
-" Map <Plug> to an <SID> function
-noremap <silent> <unique> <script>
-  \ <Plug>DubsHtmlEntities_HtmlCharTable
-  \ :call <SID>HtmlCharTable_Wrapper()<CR>
-" And finally thunk to the script fcn.
-function <SID>HtmlCharTable_Wrapper()
-  call s:HtmlCharTable()
-endfunction
+" Open HTML Character Entity Table
+" - ISOFF/2024-12-10: See toggle, below.
+"
+" if !hasmapto('<Plug>DubsHtmlEntities_HtmlCharTable')
+"   map <silent> <unique> <Leader>dh
+"     \ <Plug>DubsHtmlEntities_HtmlCharTable
+" endif
+" " Map <Plug> to an <SID> function
+" noremap <silent> <unique> <script>
+"   \ <Plug>DubsHtmlEntities_HtmlCharTable
+"   \ :call <SID>HtmlCharTable_Wrapper()<CR>
+" " And finally thunk to the script fcn.
+" function <SID>HtmlCharTable_Wrapper()
+"   call s:HtmlCharTable()
+" endfunction
 
 " ---------------
-" Run Inline HTML Entity Converter with <Leader>dH
+" Run Inline HTML Entity Converter
 " - HSTRY/2024-12-09: Was <Leader>hT but this feature is rarely
 "   used, and I've been moving most Dubs maps under \d prefix.
 if !hasmapto('<Plug>DubsHtmlEntities_QuickLookup')
   map <silent> <unique> <Leader>dH
     \ <Plug>DubsHtmlEntities_QuickLookup
+  imap <silent> <unique> <Leader>dH
+    \ <C-O><Plug>DubsHtmlEntities_QuickLookup
 endif
 " Map <Plug>
 noremap <silent> <unique> <script>
@@ -178,7 +180,7 @@ endif
 "        like a lot of work, though... so, for now, most mappings
 "        are hard-coded and not overwriteable.
 
-" Alt-Shift-5 // Toggle HTML Char Table
+" Toggle HTML Char Table
 " --------------------------------
 " This also isn't in EditPlus, but it's
 " similar to the Alt-Shift-1 Cliptext
@@ -190,9 +192,11 @@ endif
 "     imap <M-%> <C-O><Plug>DubsHtmlEntities_ToggleLookup<ESC>
 "     " cmap <M-%> <C-C><Plug>DubsHtmlEntities_ToggleLookup<ESC>
 "     " omap <M-%> <C-C><Plug>DubsHtmlEntities_ToggleLookup<ESC>
-" SYNC_ME: Dubs Vim's <M-????> mappings are spread across plugins. [\cT]
-nmap <Leader>cT <Plug>DubsHtmlEntities_ToggleLookup
-imap <Leader>cT <C-O><Plug>DubsHtmlEntities_ToggleLookup<ESC>
+" - HSTRY/2024-12-09: Was <Leader>ht to show table, and <Leader>cT
+"   to toggle visibilty, but we only need one command, also I've
+"   been moving most Dubs maps under \d prefix.
+nmap <Leader>dh <Plug>DubsHtmlEntities_ToggleLookup
+imap <Leader>dh <C-O><Plug>DubsHtmlEntities_ToggleLookup<ESC>
 
 " ------------------------------------------
 " Private Interface:

commit 3ea148cb8b5555760da5b04a0412665fe3b62a6c

Bugfix: Repair change-base functionality
- As broken by:

  3b14aa2 2024-12-11 Landon Bouma <1.1.0> Update: Use <Leader>d prefix — \dh opens HTML table, \dH runs prompt

plugin/dubs_html_entities.vim
@@ -118,11 +118,7 @@ let plugin_htmlchartable_vim = 1
 " " Map <Plug> to an <SID> function
 " noremap <silent> <unique> <script>
 "   \ <Plug>DubsHtmlEntities_HtmlCharTable
-"   \ :call <SID>HtmlCharTable_Wrapper()<CR>
-" " And finally thunk to the script fcn.
-" function <SID>HtmlCharTable_Wrapper()
-"   call s:HtmlCharTable()
-" endfunction
+"   \ :call <SID>HtmlCharTable()<CR>
 
 " ---------------
 " Run Inline HTML Entity Converter
@@ -750,7 +746,7 @@ function <SID>ToggleBase(direction)
   endif
 
   " Redraw the table using the new base
-  call <SID>HtmlCharTable_Wrapper()
+  call s:HtmlCharTable()
 
   " Restore the cursor position.
   " NOTE Calling just cursor() centers the cursor